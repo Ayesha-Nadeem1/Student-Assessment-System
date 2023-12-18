5ed36073f89963a36abe73a43d3846eade232a48
Ayesha Nadeem <ashnadeem123@gmail.com>
2023-12-18 19:48:07 +0500

    Update CRUD student

---
+++ updateStudent.Designer.cs
@@ -0,0 +1,350 @@
+﻿namespace studentAssessmentProject
+{
+    partial class updateStudent
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label5 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.txtfname = new System.Windows.Forms.TextBox();
+            this.cbStatus = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtRegNo = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtContact = new System.Windows.Forms.TextBox();
+            this.txtlname = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.checkButton = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("HP Simplified Hans", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(3, 132);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(79, 22);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Contact";
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 1;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.Controls.Add(this.btnAdd, 0, 0);
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Right;
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(643, 264);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 1;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(132, 32);
+            this.tableLayoutPanel3.TabIndex = 7;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnAdd.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.btnAdd.Location = new System.Drawing.Point(19, 5);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(94, 21);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "UPDATE";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // txtfname
+            // 
+            this.txtfname.Location = new System.Drawing.Point(130, 76);
+            this.txtfname.Name = "txtfname";
+            this.txtfname.Size = new System.Drawing.Size(218, 20);
+            this.txtfname.TabIndex = 9;
+            // 
+            // cbStatus
+            // 
+            this.cbStatus.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbStatus.FormattingEnabled = true;
+            this.cbStatus.Items.AddRange(new object[] {
+            "Active",
+            "Inactive"});
+            this.cbStatus.Location = new System.Drawing.Point(130, 236);
+            this.cbStatus.Name = "cbStatus";
+            this.cbStatus.Size = new System.Drawing.Size(121, 21);
+            this.cbStatus.TabIndex = 14;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Black;
+            this.label1.Font = new System.Drawing.Font("HP Simplified Hans", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(19, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(778, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "UPDATE STUDENT";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtRegNo
+            // 
+            this.txtRegNo.Location = new System.Drawing.Point(130, 191);
+            this.txtRegNo.Name = "txtRegNo";
+            this.txtRegNo.Size = new System.Drawing.Size(218, 20);
+            this.txtRegNo.TabIndex = 13;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(130, 162);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(218, 20);
+            this.txtEmail.TabIndex = 12;
+            // 
+            // txtContact
+            // 
+            this.txtContact.Location = new System.Drawing.Point(130, 135);
+            this.txtContact.Name = "txtContact";
+            this.txtContact.Size = new System.Drawing.Size(218, 20);
+            this.txtContact.TabIndex = 11;
+            // 
+            // txtlname
+            // 
+            this.txtlname.Location = new System.Drawing.Point(130, 107);
+            this.txtlname.Name = "txtlname";
+            this.txtlname.Size = new System.Drawing.Size(218, 20);
+            this.txtlname.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("HP Simplified Hans", 14F, System.Drawing.FontStyle.Bold);
+            this.label4.Location = new System.Drawing.Point(3, 104);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(106, 22);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Last Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label3.Font = new System.Drawing.Font("HP Simplified Hans", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(3, 73);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(121, 31);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "FIRST NAME";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("HP Simplified Hans", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(3, 159);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(60, 22);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "Email";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("HP Simplified Hans", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(3, 188);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(121, 44);
+            this.label7.TabIndex = 5;
+            this.label7.Text = "Registration Number";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("HP Simplified Hans", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(3, 233);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(69, 22);
+            this.label8.TabIndex = 6;
+            this.label8.Text = "Status";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(130, 302);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(645, 125);
+            this.dataGridView1.TabIndex = 15;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 127F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Controls.Add(this.label2, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.txtRegNo, 1, 6);
+            this.tableLayoutPanel2.Controls.Add(this.txtEmail, 1, 5);
+            this.tableLayoutPanel2.Controls.Add(this.txtContact, 1, 4);
+            this.tableLayoutPanel2.Controls.Add(this.txtlname, 1, 3);
+            this.tableLayoutPanel2.Controls.Add(this.label4, 0, 3);
+            this.tableLayoutPanel2.Controls.Add(this.label3, 0, 2);
+            this.tableLayoutPanel2.Controls.Add(this.label5, 0, 4);
+            this.tableLayoutPanel2.Controls.Add(this.label6, 0, 5);
+            this.tableLayoutPanel2.Controls.Add(this.label7, 0, 6);
+            this.tableLayoutPanel2.Controls.Add(this.label8, 0, 7);
+            this.tableLayoutPanel2.Controls.Add(this.tableLayoutPanel3, 1, 8);
+            this.tableLayoutPanel2.Controls.Add(this.txtfname, 1, 2);
+            this.tableLayoutPanel2.Controls.Add(this.cbStatus, 1, 7);
+            this.tableLayoutPanel2.Controls.Add(this.dataGridView1, 1, 9);
+            this.tableLayoutPanel2.Controls.Add(this.txtID, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.checkButton, 1, 1);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(19, 34);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 10;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 34F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 39F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 31F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 28F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 27F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 29F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 45F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 28F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 38F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 131F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(778, 413);
+            this.tableLayoutPanel2.TabIndex = 1;
+            this.tableLayoutPanel2.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel2_Paint);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Font = new System.Drawing.Font("HP Simplified Hans", 14F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(3, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(121, 34);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "ID";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(130, 3);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(218, 20);
+            this.txtID.TabIndex = 16;
+            // 
+            // checkButton
+            // 
+            this.checkButton.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.checkButton.Location = new System.Drawing.Point(130, 37);
+            this.checkButton.Name = "checkButton";
+            this.checkButton.Size = new System.Drawing.Size(121, 23);
+            this.checkButton.TabIndex = 18;
+            this.checkButton.Text = "CHECK EXISTENCE ";
+            this.checkButton.UseVisualStyleBackColor = false;
+            this.checkButton.Click += new System.EventHandler(this.checkButton_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.BackColor = System.Drawing.Color.Black;
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 8F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 8F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 2, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 405F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // updateStudent
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "updateStudent";
+            this.Text = "updateStudent";
+            this.Load += new System.EventHandler(this.updateStudent_Load);
+            this.tableLayoutPanel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox txtfname;
+        private System.Windows.Forms.ComboBox cbStatus;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtRegNo;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtContact;
+        private System.Windows.Forms.TextBox txtlname;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.Button checkButton;
+    }
+}
---
+++ updateStudent.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace studentAssessmentProject
+{
+    public partial class updateStudent : Form
+    {
+        public updateStudent()
+        {
+            InitializeComponent();
+        }
+
+        private void updateStudent_Load(object sender, EventArgs e)
+        {
+            var con1 = Configuration.getInstance().getConnection();
+            SqlCommand cmd1 = new SqlCommand("Select * from [Student]", con1);
+            SqlDataAdapter da = new SqlDataAdapter(cmd1);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+            dataGridView1.ForeColor = Color.Black;
+            dataGridView1.Font = new Font("Century Gothic", 10f);
+            cmd1.ExecuteNonQuery();
+
+            txtfname.ReadOnly = true;
+            txtlname.ReadOnly = true;
+            txtEmail.ReadOnly = true;
+            txtContact.ReadOnly = true;
+            txtRegNo.ReadOnly = true;
+            cbStatus.Enabled = false;
+            btnAdd.Enabled = false;
+        }
+
+        private void checkButton_Click(object sender, EventArgs e)
+        {
+            string value = txtID.Text;
+            //if (dataGridView1.Rows.Contains(value))
+            // {
+
+            // }
+            //if (dataGridView1.Rows.Cast<DataGridViewRow>().Any(row => row.Cells[0].Value.ToString() == value))
+            //{
+            //    MessageBox.Show("Student Found!");
+            //}
+            if (dataGridView1.Rows.Cast<DataGridViewRow>().Any(r => r.Cells[0].Value != null && r.Cells[0].Value.ToString() == value))
+            {
+                MessageBox.Show("Student Found!");
+                txtID.ReadOnly = true;
+                txtfname.ReadOnly = false;
+                txtlname.ReadOnly = false;
+                txtEmail.ReadOnly = false;
+                txtContact.ReadOnly = false;
+                txtRegNo.ReadOnly = false;
+                cbStatus.Enabled = true;
+                btnAdd.Enabled = true;
+
+                DataGridViewRow row = dataGridView1.Rows.Cast<DataGridViewRow>().Where(r => r.Cells[0].Value != null && r.Cells[0].Value.ToString() == value).First();
+                //string name = row.Cells[1].Value.ToString();
+                //string address = row.Cells[2].Value.ToString();
+                int index = dataGridView1.Rows.IndexOf(row);
+                DataGridViewRow dataRow = dataGridView1.Rows[index];
+                txtfname.Text = dataRow.Cells[1].Value.ToString();
+                txtlname.Text = dataRow.Cells[2].Value.ToString();
+                txtContact.Text = dataRow.Cells[3].Value.ToString(); 
+                txtEmail.Text = dataRow.Cells[4].Value.ToString(); 
+                txtRegNo.Text = dataRow.Cells[5].Value.ToString();
+
+                int status = Int32.Parse(dataRow.Cells[6].Value.ToString());
+                if (status ==5)
+                {
+                    cbStatus.Text = "Active";
+                }
+                else
+                {
+                    cbStatus.Text = "Inactive";
+                }
+
+            }
+            else 
+            {
+                MessageBox.Show("Student NOT Found!");
+                txtID.ReadOnly= false;
+                txtfname.ReadOnly=true;
+                txtlname.ReadOnly=true;
+                txtEmail.ReadOnly=true;
+                txtContact.ReadOnly=true;
+                txtRegNo.ReadOnly=true;
+                cbStatus.Enabled=false;
+                btnAdd.Enabled=false;
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            int status = 0;
+            var con = Configuration.getInstance().getConnection();
+            int value = Int32.Parse(txtID.Text);
+            SqlCommand cmd = new SqlCommand("SELECT Id,Contact, Email, RegistrationNumber FROM Student", con);
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                if ((reader["Contact"].ToString() == txtContact.Text || reader["Email"].ToString() == txtEmail.Text || reader["RegistrationNumber"].ToString() == txtRegNo.Text) && reader["Id"].ToString() != txtID.Text)
+                {
+                    MessageBox.Show("This contact, email or registration number already exists!");
+                    reader.Close();
+                    return;
+                }
+                else if (txtContact.Text == string.Empty || txtEmail.Text == string.Empty || txtRegNo.Text == string.Empty || txtfname.Text == string.Empty || txtlname.Text == string.Empty || cbStatus.Text == string.Empty)
+                {
+                    MessageBox.Show("No field shoud be empty!");
+                    reader.Close();
+                    return;
+                }
+                else
+                {
+                    cmd = new SqlCommand("UPDATE Student SET FirstName = @FirstName, LastName = @LastName, Contact = @Contact, Email = @Email, RegistrationNumber = @RegistrationNumber, Status = @Status WHERE Id = @Id", con);
+                    cmd.Parameters.AddWithValue("@FirstName", txtfname.Text);
+                    cmd.Parameters.AddWithValue("@LastName", txtlname.Text);
+                    cmd.Parameters.AddWithValue("@Contact", txtContact.Text);
+                    cmd.Parameters.AddWithValue("@Email", txtEmail.Text);
+                    cmd.Parameters.AddWithValue("@RegistrationNumber", txtRegNo.Text);
+                    if (cbStatus.Text == "Active")
+                    {
+                        status = 5;
+                    }
+                    else
+                    {
+                        status = 6;
+                    }
+                    cmd.Parameters.AddWithValue("@Status", status);
+                    cmd.Parameters.AddWithValue("@Id", txtID.Text);
+                }
+            }
+            reader.Close();
+
+            cmd.ExecuteNonQuery();
+            MessageBox.Show("Successfully updated");
+
+            var con1 = Configuration.getInstance().getConnection();
+            SqlCommand cmd1 = new SqlCommand("Select * from [Student]", con1);
+            SqlDataAdapter da = new SqlDataAdapter(cmd1);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+            dataGridView1.ForeColor = Color.Black;
+            dataGridView1.Font = new Font("Century Gothic", 10f);
+            cmd1.ExecuteNonQuery();
+
+        }
+
+        private void tableLayoutPanel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
