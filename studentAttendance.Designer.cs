6a40334921135219cdfcc546648b0ce1fe31b60d
Ayesha Nadeem <ashnadeem123@gmail.com>
2023-12-18 20:56:05 +0500

    Add Student Attendance

---
+++ studentAttendance.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace studentAssessmentProject
+{
+    partial class studentAttendance
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.cbAttendanceDate = new System.Windows.Forms.ComboBox();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Font = new System.Drawing.Font("HP Simplified Hans", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(19, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(778, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "STUDENT ATTENDANCE";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 8F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 8F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.label1, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 2, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 405F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
+            this.tableLayoutPanel1.TabIndex = 5;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Controls.Add(this.dataGridView1, 0, 2);
+            this.tableLayoutPanel2.Controls.Add(this.btnSave, 0, 4);
+            this.tableLayoutPanel2.Controls.Add(this.cbAttendanceDate, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.btnLoad, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.btnCheck, 0, 3);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(19, 34);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 5;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 28F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 38F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 252F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 40F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 46F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(778, 413);
+            this.tableLayoutPanel2.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 78);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(753, 228);
+            this.dataGridView1.TabIndex = 15;
+            this.dataGridView1.CellValidating += new System.Windows.Forms.DataGridViewCellValidatingEventHandler(this.dataGridView1_CellValidating);
+            this.dataGridView1.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellValueChanged);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.btnSave.BackColor = System.Drawing.Color.Silver;
+            this.btnSave.Location = new System.Drawing.Point(342, 361);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(94, 21);
+            this.btnSave.TabIndex = 16;
+            this.btnSave.Text = "SAVE";
+            this.btnSave.UseVisualStyleBackColor = false;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // cbAttendanceDate
+            // 
+            this.cbAttendanceDate.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.cbAttendanceDate.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbAttendanceDate.FormattingEnabled = true;
+            this.cbAttendanceDate.Location = new System.Drawing.Point(296, 3);
+            this.cbAttendanceDate.Name = "cbAttendanceDate";
+            this.cbAttendanceDate.Size = new System.Drawing.Size(185, 21);
+            this.cbAttendanceDate.TabIndex = 17;
+            this.cbAttendanceDate.SelectedIndexChanged += new System.EventHandler(this.cbAttendanceDate_SelectedIndexChanged);
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnLoad.BackColor = System.Drawing.Color.Silver;
+            this.btnLoad.Location = new System.Drawing.Point(335, 36);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(107, 21);
+            this.btnLoad.TabIndex = 18;
+            this.btnLoad.Text = "LOAD STUDENTS";
+            this.btnLoad.UseVisualStyleBackColor = false;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnCheck.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.btnCheck.Location = new System.Drawing.Point(343, 326);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(91, 23);
+            this.btnCheck.TabIndex = 19;
+            this.btnCheck.Text = "CHECK";
+            this.btnCheck.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnCheck.UseVisualStyleBackColor = false;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // studentAttendance
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "studentAttendance";
+            this.Text = "studentAttendance";
+            this.Load += new System.EventHandler(this.studentAttendance_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.ComboBox cbAttendanceDate;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnCheck;
+    }
+}
---
+++ studentAttendance.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace studentAssessmentProject
+{
+    public partial class studentAttendance : Form
+    {
+        DataGridViewComboBoxColumn comboBoxColumn = new DataGridViewComboBoxColumn();
+
+        public studentAttendance()
+        {
+            InitializeComponent();
+        }
+
+        private void studentAttendance_Load(object sender, EventArgs e)
+        {
+            var con = Configuration.getInstance().getConnection();
+            SqlCommand cmd = new SqlCommand("SELECT AttendanceDate FROM  ClassAttendance WHERE ClassAttendance.Id NOT IN (select AttendanceId from StudentAttendance)", con);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            cbAttendanceDate.DataSource = dt;
+            cbAttendanceDate.ValueMember = "AttendanceDate";
+            dataGridView1.AllowUserToAddRows = false;
+            btnSave.Enabled = false;
+            btnCheck.Enabled = false;   
+        }
+
+        private void cbAttendanceDate_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //if (cbAttendanceDate.SelectedIndex == -1) //No item has been selected
+            //{
+            //    btnLoad.Enabled = false;
+            //    btnSave.Enabled = false;
+            //}
+            //else  //An item has been selected
+            //{
+            //    btnLoad.Enabled = true;
+            //    btnSave.Enabled = true;
+            //}
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            cbAttendanceDate.Enabled = false;
+            btnCheck.Enabled = true;
+            //DateTime value = DateTime.Parse(cbAttendanceDate.Text);
+            //string query = "SELECT * FROM table WHERE column = @value";
+            var con = Configuration.getInstance().getConnection();
+            //SqlCommand cmd = new SqlCommand("Select * from Student,ClassAttendance WHERE AttendanceDate=@value", con);
+            //SqlCommand cmd = new SqlCommand("Select * from Student,ClassAttendance", con);
+            //DateTime date = DateTime.Parse(cbAttendanceDate.Text);
+            //cmd.Parameters.AddWithValue("@value", date);
+            SqlCommand command = new SqlCommand("Select * from Student,ClassAttendance WHERE AttendanceDate=@DateSeached AND Status = 5", con);
+
+            DateTime dateTimeValue = DateTime.Parse(cbAttendanceDate.SelectedValue.ToString());
+            command.Parameters.Add("@DateSeached", SqlDbType.DateTime).Value = dateTimeValue.ToString("yyyy-MM-dd HH:mm:ss.fff");
+
+            DataTable dataTable = new DataTable();
+            SqlDataAdapter adapter = new SqlDataAdapter(command);
+            adapter.Fill(dataTable);
+            dataGridView1.DataSource = dataTable;
+            dataGridView1.ForeColor = Color.Black;
+            dataGridView1.Font = new Font("Century Gothic", 10f);
+                                   
+            
+            if (!(dataGridView1.Columns.Contains(comboBoxColumn)))
+            {
+                //Set the properties of the ComboBox
+                comboBoxColumn.DisplayStyle = (DataGridViewComboBoxDisplayStyle)ComboBoxStyle.DropDownList;
+                comboBoxColumn.Items.AddRange(new string[] { "Present", "Absent", "Leave", "Late" });
+                comboBoxColumn.ValueType = typeof(string);
+                comboBoxColumn.DisplayStyleForCurrentCellOnly = true;
+
+                //Add validation to the column
+                DataGridViewCell cell = comboBoxColumn.CellTemplate;
+                //cell.Value = "";
+                cell.ErrorText = "Value not selected";
+
+                //Add to the DataGridView
+                dataGridView1.Columns.Add(comboBoxColumn);
+            }
+            
+            
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+
+            //MessageBox.Show("All OK :)");
+            var con = Configuration.getInstance().getConnection();
+            int status = 0;
+            //SqlCommand cmd = new SqlCommand("INSERT INTO StudentAttendance VALUES (@value1, @value2, @value3)", con);
+            //Open the connection
+            //con.Open();
+
+            //Iterate through the DataGridView rows
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                //Create a new SqlCommand object
+                SqlCommand cmd = new SqlCommand("INSERT INTO StudentAttendance VALUES (@value1, @value2, @value3)",con);
+                //cmd.Connection = con;
+                //cmd.CommandType = CommandType.Text;
+                //cmd.CommandText = "INSERT INTO dbo.TableName VALUES (@param1, @param2, ...)";
+
+                //Add the parameters
+                if(row.Cells[9].Value.ToString() == "Present")
+                {
+                    MessageBox.Show("1");
+                   status = 1;
+                }
+                if (row.Cells[9].Value.ToString() == "Absent")
+                {
+                    MessageBox.Show("2");
+                    status = 2;
+                }
+                if (row.Cells[9].Value.ToString() == "Leave")
+                {
+                    MessageBox.Show("3");
+                    status = 3;
+                }
+                if (row.Cells[9].Value.ToString() == "Late")
+                {
+                    MessageBox.Show("4");
+                    status = 4;
+                }
+                cmd.Parameters.AddWithValue("@value1", row.Cells[7].Value);
+                cmd.Parameters.AddWithValue("@value2", row.Cells[0].Value);
+                cmd.Parameters.AddWithValue("@value3", status);
+                //string value = 
+                //cmd.Parameters.AddWithValue("@value3", row.Cells[1].Value);
+                //...
+
+                //Execute the command
+                cmd.ExecuteNonQuery();
+            }
+            //cmd.ExecuteNonQuery();
+            //Close the connection
+            //con.Close();
+            btnSave.Enabled = false;
+            btnCheck.Enabled = false;
+            SqlCommand cmd1 = new SqlCommand("SELECT AttendanceDate FROM  ClassAttendance WHERE ClassAttendance.Id NOT IN (select AttendanceId from StudentAttendance)", con);
+            SqlDataAdapter da = new SqlDataAdapter(cmd1);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            cbAttendanceDate.DataSource = dt;
+            cbAttendanceDate.ValueMember = "AttendanceDate";
+            dataGridView1.AllowUserToAddRows = false;
+            cbAttendanceDate.Enabled = true;
+        }
+
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+            bool allCellsValid = true;
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                DataGridViewCell cell = row.Cells[comboBoxColumn.Index];
+                if (!String.IsNullOrEmpty(cell.ErrorText)) //if error text is not null then the IF condition is true 
+                {
+                    allCellsValid = false;
+                    MessageBox.Show("Select Attendance Status for every student!");
+                    break;
+                }
+                
+            }
+
+            if (allCellsValid)
+            {
+                btnSave.Enabled = true;
+            }
+        }
+
+        private void dataGridView1_CellValidating(object sender, DataGridViewCellValidatingEventArgs e)
+        {
+            //if (e.ColumnIndex == 0) //column index of combobox
+            //{ 
+            //    if (String.IsNullOrEmpty(e.FormattedValue.ToString())) 
+            //    { MessageBox.Show("Please select a value"); 
+            //     e.Cancel = true; 
+            //    }
+            //}
+        }
+
+        private void dataGridView1_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            DataGridViewCell cell = dataGridView1[e.ColumnIndex, e.RowIndex];
+            if (cell.ColumnIndex == comboBoxColumn.Index)
+            {
+                cell.ErrorText = "";
+            }
+        }
+    }
+}
