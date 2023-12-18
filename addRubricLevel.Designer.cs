46ce6608e71d018a603a365afea0b694a4b48490
Ayesha Nadeem <ashnadeem123@gmail.com>
2023-12-18 20:26:25 +0500

    Add CRUD Rubric Level

---
+++ addRubricLevel.Designer.cs
@@ -0,0 +1,271 @@
+﻿namespace studentAssessmentProject
+{
+    partial class addRubricLevel
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.cbML = new System.Windows.Forms.ComboBox();
+            this.cbRubricID = new System.Windows.Forms.ComboBox();
+            this.txtDetails = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Font = new System.Drawing.Font("HP Simplified Hans", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(19, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(778, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ADD RUBRIC LEVEL";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 8F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 8F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.label1, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 2, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 405F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
+            this.tableLayoutPanel1.TabIndex = 4;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 192F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Controls.Add(this.label2, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.label3, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.label6, 0, 3);
+            this.tableLayoutPanel2.Controls.Add(this.tableLayoutPanel3, 1, 4);
+            this.tableLayoutPanel2.Controls.Add(this.txtID, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.dataGridView1, 1, 5);
+            this.tableLayoutPanel2.Controls.Add(this.cbML, 1, 3);
+            this.tableLayoutPanel2.Controls.Add(this.cbRubricID, 1, 1);
+            this.tableLayoutPanel2.Controls.Add(this.txtDetails, 1, 2);
+            this.tableLayoutPanel2.Controls.Add(this.label4, 0, 2);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(19, 34);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 6;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 28F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 24F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 32F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 54F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 36F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 102F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(778, 413);
+            this.tableLayoutPanel2.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Font = new System.Drawing.Font("HP Simplified Hans", 14F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(3, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(186, 28);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "ID";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label3.Font = new System.Drawing.Font("HP Simplified Hans", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(3, 28);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(186, 24);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "RUBRIC ID";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label6.Font = new System.Drawing.Font("HP Simplified Hans", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(3, 84);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(186, 54);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "MEASUREMENT LEVEL";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 1;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.Controls.Add(this.btnAdd, 0, 0);
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Right;
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(643, 141);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 1;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(132, 30);
+            this.tableLayoutPanel3.TabIndex = 7;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnAdd.BackColor = System.Drawing.Color.Silver;
+            this.btnAdd.Location = new System.Drawing.Point(19, 4);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(94, 21);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "ADD";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(195, 3);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(218, 20);
+            this.txtID.TabIndex = 8;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(195, 177);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(580, 233);
+            this.dataGridView1.TabIndex = 15;
+            // 
+            // cbML
+            // 
+            this.cbML.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbML.FormattingEnabled = true;
+            this.cbML.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4"});
+            this.cbML.Location = new System.Drawing.Point(195, 87);
+            this.cbML.Name = "cbML";
+            this.cbML.Size = new System.Drawing.Size(121, 21);
+            this.cbML.TabIndex = 17;
+            this.cbML.SelectedValueChanged += new System.EventHandler(this.cbML_SelectedValueChanged);
+            // 
+            // cbRubricID
+            // 
+            this.cbRubricID.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbRubricID.FormattingEnabled = true;
+            this.cbRubricID.Location = new System.Drawing.Point(195, 31);
+            this.cbRubricID.Name = "cbRubricID";
+            this.cbRubricID.Size = new System.Drawing.Size(121, 21);
+            this.cbRubricID.TabIndex = 18;
+            // 
+            // txtDetails
+            // 
+            this.txtDetails.Location = new System.Drawing.Point(195, 55);
+            this.txtDetails.Name = "txtDetails";
+            this.txtDetails.Size = new System.Drawing.Size(218, 20);
+            this.txtDetails.TabIndex = 19;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label4.Font = new System.Drawing.Font("HP Simplified Hans", 14.25F, System.Drawing.FontStyle.Bold);
+            this.label4.Location = new System.Drawing.Point(3, 52);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(186, 32);
+            this.label4.TabIndex = 20;
+            this.label4.Text = "DETAILS";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // addRubricLevel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "addRubricLevel";
+            this.Text = "addRubricLevel";
+            this.Load += new System.EventHandler(this.addRubricLevel_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel2.PerformLayout();
+            this.tableLayoutPanel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox cbML;
+        private System.Windows.Forms.ComboBox cbRubricID;
+        private System.Windows.Forms.TextBox txtDetails;
+        private System.Windows.Forms.Label label4;
+    }
+}
---
+++ addRubricLevel.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace studentAssessmentProject
+{
+    public partial class addRubricLevel : Form
+    {
+        public addRubricLevel()
+        {
+            InitializeComponent();
+        }
+
+        private void addRubricLevel_Load(object sender, EventArgs e)
+        {
+            var con = Configuration.getInstance().getConnection();
+            SqlCommand cmd = new SqlCommand("Select * from [RubricLevel]", con);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+            dataGridView1.ForeColor = Color.Black;
+            dataGridView1.Font = new Font("Century Gothic", 10f);
+            int count = 0;
+
+            cmd = new SqlCommand("SELECT COUNT(*) FROM [ProjectB].[dbo].[RubricLevel]", con);
+            count = (Int32)cmd.ExecuteScalar();
+            count++;
+            txtID.Text = count.ToString();
+
+            cmd = new SqlCommand("SELECT Id FROM  Rubric", con);
+            da = new SqlDataAdapter(cmd);
+            dt = new DataTable();
+            da.Fill(dt);
+            cbRubricID.DataSource = dt;
+            cbRubricID.ValueMember = "Id";
+            dataGridView1.AllowUserToAddRows = false;
+            txtDetails.ReadOnly= true;
+        }
+
+        private void cbML_SelectedValueChanged(object sender, EventArgs e)
+        {
+            int value = int.Parse(cbML.Text);
+            if (value == 1)
+            {
+                txtDetails.Text = "Unsatisfactory";
+            }
+            else if (value == 2)
+            {
+                txtDetails.Text = "Fair";
+            }
+            else if (value == 3)
+            {
+                txtDetails.Text = "Good";
+            }
+            else if (value == 4)
+            {
+                txtDetails.Text = "Exceptional";
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            var con = Configuration.getInstance().getConnection();
+            SqlCommand cmd = new SqlCommand("SELECT * FROM RubricLevel", con);
+            SqlDataReader reader = cmd.ExecuteReader();
+            if (txtID.Text == string.Empty || txtDetails.Text == string.Empty || cbRubricID.SelectedIndex == -1 || cbML.SelectedIndex == -1)
+            {
+                MessageBox.Show("No field shoud be empty!");
+                reader.Close();
+                return;
+            }
+            while (reader.Read())
+            {
+                if (reader["Id"].ToString() == txtID.Text)
+                {
+                    MessageBox.Show("This ID already exists!");
+                    reader.Close();
+                    return;
+                }
+                if (reader["RubricId"].ToString() == cbRubricID.Text && reader["MeasurementLevel"].ToString() == cbML.Text)
+                {
+                    MessageBox.Show("This MeasurementLevel already exists for selected Rubric ID!");
+                    reader.Close();
+                    return;
+                }
+            }
+            reader.Close();
+            cmd.Connection = con;
+
+
+            cmd.CommandText = "SET IDENTITY_INSERT RubricLevel ON";
+            cmd.ExecuteNonQuery();
+            //cmd.Connection = con;
+            //cmd.CommandText = "INSERT INTO RubricLevel (Id,RubricId,Details,MeasurementLevel) VALUES (Id,RubricId,Details,MeasurementLevel)";
+
+            cmd = new SqlCommand("INSERT INTO RubricLevel (Id,RubricId,Details,MeasurementLevel) VALUES (@Id,@RubricId,@Details,@MeasurementLevel)", con);
+            cmd.Parameters.AddWithValue("@Id", txtID.Text);
+            cmd.Parameters.AddWithValue("@RubricId", cbRubricID.Text);
+            cmd.Parameters.AddWithValue("@Details", txtDetails.Text);
+            cmd.Parameters.AddWithValue("@MeasurementLevel", cbML.Text);
+            cmd.ExecuteNonQuery();
+
+            cmd.CommandText = "SET IDENTITY_INSERT RubricLevel OFF";
+            cmd.ExecuteNonQuery();
+
+            cmd = new SqlCommand("Select * FROM RubricLevel", con);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+            dataGridView1.ForeColor = Color.Black;
+            dataGridView1.Font = new Font("Century Gothic", 10f);
+            cmd.ExecuteNonQuery();
+
+        }
+    }
+}
